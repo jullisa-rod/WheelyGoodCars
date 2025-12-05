db133f8165cf42e577e69f1677ca33ed9268e207
jullierodriguez <jullisa.rodriguez4@gmail.com>
2025-12-05 15:50:42 -0800

    start

---
+++ mydealership/src/main/java/com/pluralsight/Trip.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.io.*;
+
+public class Trip {
+    //LocalDate date1 = LocalDate.of(year: 2022, Month.MARCH, dayOfMonth: 21);
+}
